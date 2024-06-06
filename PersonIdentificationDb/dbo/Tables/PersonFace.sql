456af8e1c3dfa757cab624619503c19daf520bcc
William Xifaras <wxifaras@microsoft.com>
2024-06-05 21:55:27 -0400

    fixes

---
+++ PersonIdentificationDb/dbo/Tables/PersonFace.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PersonFace]
+(
+	[FaceId]             UNIQUEIDENTIFIER NOT NULL,
+    [PersonId]           UNIQUEIDENTIFIER NOT NULL,
+	[CreatedBy]          VARCHAR (50)     NOT NULL DEFAULT 'system',
+    [CreatedDate]        DATETIME         DEFAULT (getutcdate()) NOT NULL,
+    [ModifiedBy]         VARCHAR (50)     NULL,
+    [ModifiedDate]       DATETIME         NULL,
+    PRIMARY KEY CLUSTERED ([FaceId] ASC),
+    FOREIGN KEY ([PersonId]) REFERENCES [dbo].[PersonGroupImage] ([PersonId])
+);
