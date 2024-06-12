bca619719a1a90b0bdeda264f3617484edbcdb2f
William Xifaras <12738175+wxifaras@users.noreply.github.com>
2024-06-12 12:29:20 -0400

    Update PersonGroupPersonFace.sql

--- PersonIdentificationDb/dbo/Tables/PersonGroupPersonFace.sql
+++ PersonIdentificationDb/dbo/Tables/PersonGroupPersonFace.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[PersonGroupPersonFace]
 (
-	[FaceId]             UNIQUEIDENTIFIER NOT NULL,
+    [FaceId]             UNIQUEIDENTIFIER NOT NULL,
     [PersonId]           UNIQUEIDENTIFIER NOT NULL,
     [BlobName]           VARCHAR (100)    NOT NULL,
     [BlobUrl]            VARCHAR (250)    NOT NULL,
     [IsTrained]          BIT              NULL,
-	[CreatedBy]          VARCHAR (50)     NOT NULL DEFAULT 'system',
+    [CreatedBy]          VARCHAR (50)     NOT NULL DEFAULT 'system',
     [CreatedDate]        DATETIME         DEFAULT (getutcdate()) NOT NULL,
     [ModifiedBy]         VARCHAR (50)     NULL,
     [ModifiedDate]       DATETIME         NULL,
